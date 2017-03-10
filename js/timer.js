665dc4525d7b5077108ec18d3941c4b34fb373d3
m652326 <Dora.Do@macys.com>
2017-03-10 12:56:53 -0800

    cache images

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    preloadImages(["images/img043.jpg"]);
+
     $('button.next').click(function(){
         $(this).parents('.step').hide();
         $(this).parents('.step').next().css('display', 'table-cell');
@@ -99,3 +101,24 @@ function startDevTimer(duration, stepEl) {
 function convertSecsToTime(seconds){
     return moment().minutes(0).seconds(seconds).format('mm:ss');
 }
+
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
